6a93eb83ce47f0cc5e019039861a280fcd702081
Валентин Николаевич <valentin@vetukov.ru>
2025-08-17 12:22:32 +0300

    Complete HW 03

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,26 +1,117 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+const (
+	USDtoEUR = 0.85
+	USDtoRUB = 79.78
+	EURtoUSD = 1.16
+	EURtoRUB = (USDtoRUB / USDtoEUR) * EURtoUSD
+	RUBtoEUR = 0.010671
+	RUBtoUSD = 0.012497
+)
+
+var firstCurrency, secondCurrency string
+var cash int
+var err error
 
 func main() {
-	const (
-		USDtoEUR = 0.85
-		USDtoRUB = 79.78
-		EURtoUSD = 1.16
-		EURtoRUB = (USDtoRUB / USDtoEUR) * EURtoUSD
-	)
 
-	data := GetUserData()
-	fmt.Println(data)
+	for {
+		firstCurrency, err = GetUserCurrencyData()
+		if isValidData(err) {
+			break
+		}
+	}
 
-	fmt.Print(EURtoRUB)
+	for {
+		cash, err = GetUserCashData()
+		if isValidData(err) {
+			break
+		}
+	}
+
+	for {
+		secondCurrency, err = GetUserCurrencyData()
+		if isValidData(err) {
+			break
+		}
+	}
+
+	result := calculateIMT(cash, firstCurrency, secondCurrency)
+	fmt.Printf("%.2f", result)
+
 }
 
-func GetUserData() (data string) {
+func GetUserCurrencyData() (data string, err error) {
+	printLineForEnterCurrency()
+
 	fmt.Scan(&data)
+
+	if data != "USD" && data != "EUR" && data != "RUB" && firstCurrency == data {
+		err = errors.New("NOT_VALID")
+	}
+
 	return
 }
 
-func calculateIMT(coin int, currencySource string, currencyTarget string) {
+func GetUserCashData() (data int, err error) {
+	fmt.Print("Введите количество денег: ")
 
+	var input string
+	fmt.Scan(&input)
+
+	if data, err = strconv.Atoi(input); err != nil {
+		err = errors.New("NOT_VALID")
+	}
+
+	return
+}
+
+func isValidData(err error) bool {
+	if err != nil {
+		fmt.Println("Вы ввели не корректное значение, повторите пожалуйста попытку ввода.")
+		return false
+	}
+	return true
+}
+
+func printLineForEnterCurrency() {
+	var currencyLine string = "EUR, USD, RUB"
+
+	switch firstCurrency {
+	case "EUR":
+		currencyLine = "USD, RUB"
+	case "USD":
+		currencyLine = "EUR, RUB"
+	case "RUB":
+		currencyLine = "EUR, USD"
+	}
+
+	fmt.Printf("Введите желаемую валюту (%s): ", currencyLine)
+}
+
+func calculateIMT(coin int, currencySource string, currencyTarget string) float64 {
+	var result float64
+
+	switch {
+	case currencySource == "EUR" && currencyTarget == "RUB":
+		result = float64(coin) * EURtoRUB
+	case currencySource == "EUR" && currencyTarget == "USD":
+		result = float64(coin) * EURtoUSD
+	case currencySource == "USD" && currencyTarget == "RUB":
+		result = float64(coin) * USDtoRUB
+	case currencySource == "USD" && currencyTarget == "EUR":
+		result = float64(coin) * USDtoEUR
+	case currencySource == "RUB" && currencyTarget == "USD":
+		result = float64(coin) * RUBtoUSD
+	case currencySource == "RUB" && currencyTarget == "EUR":
+		result = float64(coin) * RUBtoEUR
+	}
+
+	return result
 }
